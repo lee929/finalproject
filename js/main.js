fd944eb2d058123b8133e0d831bb53fcbc6c69dc
lee929 <ko0kiemonster29@172-16-27-63.DYNAPOOL.NYU.EDU>
2015-12-03 20:36:36 -0500

    adding latest files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+$(document).ready(function() {
+	$(".fancybox-thumb").fancybox({
+		prevEffect	: 'none',
+		nextEffect	: 'none',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 80,
+				height	: 80
+			}
+		}
+	});
+});
+/*
+	$('.main-gallery').flickity({
+	  // options
+	  cellAlign: 'left',
+	  contain: true
+	});
 
+*/
